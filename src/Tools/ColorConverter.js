4147d312d943b70c1c753bdbf7d8d3b4fd75747b
rickisen <rickard.spam@gmail.com>
2016-06-29 20:03:38 +0200

    Add converter for hex to rgb and vice versa

--- src/Tools/ColorConverter.js
+++ src/Tools/ColorConverter.js
@@ -132,3 +132,29 @@ export function hsvToRgb(h, s, v){
   return [r * 255, g * 255, b * 255];
 }
 
+// from: http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+export function hexToRgb(hex) {
+  // Expand shorthand form (e.g. '03F') to full form (e.g. '0033FF')
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? [
+    parseInt(result[1], 16),
+    parseInt(result[2], 16),
+    parseInt(result[3], 16)
+  ] : null;
+}
+
+// from: http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+export function rgbToHex(r, g, b) {
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+// from: http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? '0' + hex : hex;
+}
