33b4058943f99ef24b818e4e4d8a4f33536b8df6
rickisen <rickard.spam@gmail.com>
2016-07-01 01:06:20 +0200

    Basic support for InjectConvertedCss

--- src/Strategies/InjectConvertedCss.js
+++ src/Strategies/InjectConvertedCss.js
@@ -1,7 +1,53 @@
-// var links = document.getElementsByTagName('link');
-//
-// for (var i = 0, len = links.length; i < len; i++) {
-//   if (links[i].getAttribute('rel') == 'stylesheet') {
-//     console.log(links[i]);
-//   }
-// }
+import { isRgbString } from '../Classes/RgbString';
+import Color           from '../Classes/Color';
+import fetch           from 'isomorphic-fetch';
+
+export default class InjectConvertedCss {
+  constructor() {
+    var links = document.getElementsByTagName('link');
+    this.stylesheets = [];
+    this.convertedStylesheets = [];
+
+    var stylesheetURIs = [];
+    for (var i = 0, len = links.length; i < len; i++) {
+      let link = links[i];
+      if (link.getAttribute('rel') == 'stylesheet') {
+        stylesheetURIs.push(link.getAttribute('href'));
+      }
+    }
+
+    stylesheetURIs.map((URI) => {
+      fetch(URI).then((responce) => {
+        responce.text().then((text) => (
+          this.stylesheets.push(text)
+        ));
+      });
+    });
+  }
+
+  convertText() {
+    this.convertedStylesheets = this.stylesheets.map((stylesheet) => (
+      stylesheet.replace(/#([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?\b/g, 'pink')
+    ));
+
+    console.log(this.convertedStylesheets);
+  }
+
+  applyEffect() {
+    this.convertText();
+
+    this.convertedStylesheets.map((convertedSS) => {
+      let elem = document.createElement('style');
+      elem.type = 'text/css';
+
+      if (elem.styleSheet) {
+        elem.styleSheet.cssText = convertedSS;
+      } else {
+        elem.appendChild(document.createTextNode(convertedSS));
+      }
+
+      document.head.appendChild(elem);
+    });
+  }
+}
+
