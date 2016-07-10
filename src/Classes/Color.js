c0e659159f23858bac0915362d5428d10a35ad7a
rickisen <rickard.spam@gmail.com>
2016-07-10 23:38:01 +0200

    Add support for rgba colors

--- src/Classes/Color.js
+++ src/Classes/Color.js
@@ -1,19 +1,26 @@
-import { isRgbString } from '../Classes/RgbString';
+import { isRgbString, isRgbaString } from '../Classes/RgbString';
 import * as ColorConverter from '../Tools/ColorConverter';
 
 export default class Color {
   constructor(colorString) {
+    this.a = 1;
 
-    if (!isRgbString(colorString)) {
+    if (isRgbString(colorString)) {
+      let colorSplit = colorString.split(',');
+      this.r = 0 + parseInt(colorSplit[0].substr(4).trim());
+      this.g = 0 + parseInt(colorSplit[1].trim());
+      this.b = 0 + parseInt(colorSplit[2].replace(')', ' ').trim());
+    } else if (isRgbaString(colorString)) {
+      let colorSplit = colorString.split(',');
+      this.r = 0 + parseInt(colorSplit[0].substr(5).trim());
+      this.g = 0 + parseInt(colorSplit[1].trim());
+      this.b = 0 + parseInt(colorSplit[2].trim());
+      this.a = 0 + parseFloat(colorSplit[3].replace(')', ' ').trim());
+    } else {
       let colorArr = ColorConverter.hexToRgb(colorString);
       this.r = colorArr[0];
       this.g = colorArr[1];
       this.b = colorArr[2];
-    } else {
-      var colorSplit = colorString.split(',');
-      this.r = 0 + parseInt(colorSplit[0].substr(4).trim());
-      this.g = 0 + parseInt(colorSplit[1].trim());
-      this.b = 0 + parseInt(colorSplit[2].replace(')', ' ').trim());
     }
   }
 
@@ -27,7 +34,7 @@ export default class Color {
     // convert back to rgb
     var darkenedRgb = ColorConverter.hslToRgb(darkenedHsl[0], darkenedHsl[1], darkenedHsl[2]);
 
-    // return it in string form
-    return 'rgb(' + darkenedRgb[0] + ', ' + darkenedRgb[1] + ', ' + darkenedRgb[2] + ')';
+    // return it in rgba string form
+    return 'rgba(' + Math.round(darkenedRgb[0]) + ', ' + Math.round(darkenedRgb[1]) + ', ' + Math.round(darkenedRgb[2]) + ', ' + this.a + ')';
   }
 }
--- src/Classes/RgbString.js
+++ src/Classes/RgbString.js
@@ -2,4 +2,8 @@ function isRgbString(suspect) {
   return suspect.lastIndexOf('rgb(', 0) === 0;
 }
 
-export { isRgbString };
+function isRgbaString(suspect) {
+  return suspect.lastIndexOf('rgba(', 0) === 0;
+}
+
+export { isRgbString, isRgbaString };
